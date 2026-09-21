dbf327ef79b8a5392ea3b5967269f4489e77b7ec
SasmitaPriyadarshiniDas <59992129+SasmitaPriyadarshiniDas@users.noreply.github.com>
2026-09-21 14:17:07 +0530

    Fix transaction service CI tests without external MySQL dependency.

--- transaction-service/src/test/java/org/natwest/transaction/TransactionServiceApplicationTests.java
+++ transaction-service/src/test/java/org/natwest/transaction/TransactionServiceApplicationTests.java
@@ -2,12 +2,13 @@ package org.natwest.transaction;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TransactionServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
