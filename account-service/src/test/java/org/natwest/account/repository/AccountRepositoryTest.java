bf50c7e0975f64eca9b8719bd0dcdd63ea7453f5
SasmitaPriyadarshiniDas <59992129+SasmitaPriyadarshiniDas@users.noreply.github.com>
2026-09-21 13:01:49 +0530

    Use JPA slice test with H2 for repository test

--- account-service/src/test/java/org/natwest/account/repository/AccountRepositoryTest.java
+++ account-service/src/test/java/org/natwest/account/repository/AccountRepositoryTest.java
@@ -2,36 +2,20 @@ package org.natwest.account.repository;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.natwest.account.AccountServiceApplication;
-import org.natwest.account.entity.Account;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.math.BigDecimal;
 import java.util.Optional;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
 
 @RunWith(SpringRunner.class)
-@ContextConfiguration(classes = AccountServiceApplication.class)
+@DataJpaTest
 public class AccountRepositoryTest {
 
     @Autowired
     private AccountRepository accountRepository;
-
-    // @Test
-    public void shouldFindAccountForUpdate() {
-        Account account = new Account("ACC1001", "John Doe", new BigDecimal("1000.00"), "GBP");
-        accountRepository.save(account);
-        Optional<Account> result = accountRepository.findByIdForUpdate("ACC1001");
-        assertTrue(result.isPresent());
-        Account foundAccount = result.get();
-        assertEquals("ACC1001", foundAccount.getAccountId());
-        assertEquals("John Doe", foundAccount.getAccountHolder());
-        assertEquals(new BigDecimal("1000.00"), foundAccount.getBalance());
-        assertEquals("GBP", foundAccount.getCurrency());
-    }
 
     @Test
     public void shouldReturnEmptyWhenAccountDoesNotExist() {
