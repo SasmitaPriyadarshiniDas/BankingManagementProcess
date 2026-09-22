18bbfbf558dd666853a62402b1485c853f780eee
SasmitaPriyadarshiniDas <59992129+SasmitaPriyadarshiniDas@users.noreply.github.com>
2026-09-22 12:06:01 +0530

    Fix missing WebClient.Builder bean for transaction-service

--- transaction-service/src/main/java/org/natwest/transaction/config/WebClientConfig.java
+++ transaction-service/src/main/java/org/natwest/transaction/config/WebClientConfig.java
@@ -8,7 +8,9 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class WebClientConfig {
 
     @Bean
-    public WebClient accountWebClient(WebClient.Builder builder) {
-        return builder.baseUrl("http://localhost:8081").build();
+    public WebClient accountWebClient() {
+        return WebClient.builder()
+                .baseUrl("http://localhost:8081")
+                .build();
     }
 }
